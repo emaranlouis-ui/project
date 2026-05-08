2cf00f6397122685e170ba66a852b73cd8876a3b
emaranlouis-ui <emaranlouisphilippe@gmail.com>
2026-05-08 16:11:28 +0100

    Delete myproject directory

--- myproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def homepage(request):
-    #return HttpResponse("Hello, Welcome to the homepage!")
-    return render(request,'home.html')
-
-def about_us(request):
-    #return HttpResponse("This is my About Us page.")
-    return render(request,'about.html')    
